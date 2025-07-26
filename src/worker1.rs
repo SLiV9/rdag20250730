cdf7156f3d49f662ecbe686a0c6bff0adcb15529
Sander in 't Veld <sanderintveld@hotmail.com>
2025-07-26 21:31:13 +0200

    Made some more progress

--- src/worker1.rs
+++ src/worker1.rs
@@ -10,6 +10,31 @@ pub struct NaiveWorker {
     greeks: Vec<InstrumentData>,
 }
 
+impl NaiveWorker {
+    pub fn update_separate(
+        &mut self,
+        id: Id,
+        delta: f32,
+        gamma: f32,
+        theta: f32,
+        vega: f32,
+    ) {
+        let greeks = Greeks {
+            delta: Delta(delta),
+            gamma: Gamma(gamma),
+            theta: Theta(theta),
+            vega: Vega(vega),
+        };
+        if let Some(entry) =
+            self.greeks.iter_mut().find(|x| x.id == id)
+        {
+            entry.greeks = greeks;
+        } else {
+            self.greeks.push(InstrumentData { id, greeks });
+        }
+    }
+}
+
 impl Worker for NaiveWorker {
     fn update(&mut self, id: Id, greeks: Greeks) {
         if let Some(entry) =
